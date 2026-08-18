e62a29b8feb7e98daf2554dbe2b7b7c80e23d850
Dat-one-dev <devselfish@gmail.com>
2026-08-18 20:44:16 +0530

    added logError func ig ts will make it less complex i useully add it in go proj forg this time lol

--- main.go
+++ main.go
@@ -9,16 +9,18 @@ import (
 
 func main() {
 	user, err := user.Current()
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
+	logError(err)
 
 	configPath := filepath.Join(user.HomeDir, ".wakatime.cfg")
 	data, err := os.ReadFile(configPath)
+	logError(err)
+
+	fmt.Println(string(data))
+}
+
+func logError(err error) {
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
-	fmt.Println(string(data))
 }
