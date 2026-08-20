da7bce4b6dbb1f4bfb9a67367d2afbfba6195621
Dat-one-dev <devselfish@gmail.com>
2026-08-20 18:26:23 +0530

    BOX APPEARS

--- tui.go
+++ tui.go
@@ -1,22 +1,38 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
 
-type model struct{}
+type model struct {
+	w int
+	h int
+}
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// QUIT
 	if key, ok := msg.(tea.KeyMsg); ok {
-		if key.String() == "q" {
+		if key.String() == "q" || key.String() == "ctrl+c" {
 			return m, tea.Quit
 		}
 	}
+
+	if size, ok := msg.(tea.WindowSizeMsg); ok {
+		m.w = size.Width
+		m.h = size.Height
+	}
+
 	return m, nil
 }
 
 func (m model) View() string {
-	return "SUSHUPTI"
+
+	box := lipgloss.NewStyle().Width(m.w-2).Height(m.h-2).Border(lipgloss.DoubleBorder()).Padding(1, 2).Render("Sushupti")
+
+	return box
 }
