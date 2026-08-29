3877f6b6c1b74123765b72de578f643bc2754652
Kartik Patel <devselfish@gmail.com>
2026-08-29 11:12:19 +0530

    add release configuration

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+REPO="Dat-one-dev/Sushupti"
+INSTALL_DIR="$HOME/.local/bin"
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+case "$OS" in
+    Linux) OS="Linux" ;;
+    Darwin) OS="Darwin" ;;
+    *) echo "Unsupported OS: $OS"; exit 1 ;;
+esac
+
+case "$ARCH" in
+    x86_64) ARCH="x86_64" ;;
+    aarch64|arm64) ARCH="arm64" ;;
+    *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+esac
+
+VERSION=$(curl -s https://api.github.com/repos/$REPO/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)
+
+URL="https://github.com/$REPO/releases/download/$VERSION/Sushupti_${OS}_${ARCH}.tar.gz"
+
+mkdir -p "$INSTALL_DIR"
+
+echo "Downloading Sushupti $VERSION..."
+
+curl -L "$URL" -o /tmp/sushupti.tar.gz
+
+tar -xzf /tmp/sushupti.tar.gz -C /tmp
+
+mv /tmp/Sushupti "$INSTALL_DIR/sushupti"
+
+chmod +x "$INSTALL_DIR/sushupti"
+
+echo ""
+echo "Sushupti installed!"
+echo "Run it with: sushupti"
